81c3b56a0389bee23be32a3c0cd8c011630c4e50
Iliyan Georgiev <georgiev.iliyan09@gmail.com>
2025-07-02 13:26:51 +0300

    styling, note displaying

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import './App.css';
+import './index.css';
 import NotesList from './components/NoteList';
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 
 function App() {
+  /* array for notes */
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note!",
+      date: "15/04/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note!",
+      date: "16/04/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note!",
+      date: "17/04/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my fourth note!",
+      date: "18/04/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my fifth note!",
+      date: "19/04/2021",
+    }
+  ]);
+
   return (
     <div className="App">
       <h1>Notey</h1>
       <div className="container">
-        <NotesList></NotesList>
+        <NotesList notes = {notes}></NotesList>
       </div>
     </div>
   );
